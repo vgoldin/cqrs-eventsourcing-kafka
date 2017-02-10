0b10e7f2dfb26850fad014700513abb7488dd816
Veniamin Goldin <veniamin@backbase.com>
2017-02-10 18:13:03 +0100

    Remove redundant methods

--- services-core/src/main/java/io/plumery/core/infrastructure/EventStore.java
+++ services-core/src/main/java/io/plumery/core/infrastructure/EventStore.java
@@ -4,7 +4,6 @@ import io.plumery.core.AggregateRoot;
 import io.plumery.core.Event;
 
 public interface EventStore {
-    void saveEvents(String aggregateId, Iterable<? extends Event> events, int expectedVersion);
     void saveEvents(String streamName, String aggregateId, Iterable<? extends Event> events, int expectedVersion);
     Iterable<? extends Event> getEventsForAggregate(Class<? extends AggregateRoot> aggregate, String aggregateId);
 }

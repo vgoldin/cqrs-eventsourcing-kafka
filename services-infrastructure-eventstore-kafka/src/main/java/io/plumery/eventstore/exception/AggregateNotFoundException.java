0a679dea7fb170c2adeb05be97a05b011585bdb7
Veniamin Goldin <veniamin@backbase.com>
2017-05-18 16:13:20 +0200

    Remove deprecated kafka eventstore

--- services-infrastructure-eventstore-kafka/src/main/java/io/plumery/eventstore/exception/AggregateNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package io.plumery.eventstore.exception;
-
-import io.plumery.core.ID;
-
-public class AggregateNotFoundException extends RuntimeException {
-    public AggregateNotFoundException(String name, ID id) {
-        super(name + "; Id=" + id.toString());
-    }
-}
--- services-infrastructure-eventstore-kafka/src/main/java/io/plumery/eventstore/exception/EventNotSupportedExeption.java
+++
@@ -1,7 +0,0 @@
-package io.plumery.eventstore.exception;
-
-/**
- * Created by veniamin on 12/02/2017.
- */
-public class EventNotSupportedExeption extends RuntimeException {
-}
